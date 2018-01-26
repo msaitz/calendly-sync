2c81dbe6605d867b6271a43b232ff55c0a40c354
Miguel Saitz <mie.saitz@gmail.com>
2018-01-26 22:13:15 +0000

    Restructured class

--- lib/calendly.py
+++ lib/calendly.py
@@ -1,33 +1,85 @@
 import requests
-import json
+import datetime
 
 
-class Calendly(object):
-    _api_key = {'X-TOKEN': 'LIMHDEAGLGPFC4XOFLYO23CPYNKOMNBD'}
-    _name = ''
-    _time = ''
-    _index = None
-    _type = None
+class CalendlyEvent:
+    def __init__(self):
+        self.api_key = {'X-TOKEN': 'LIMHDEAGLGPFC4XOFLYO23CPYNKOMNBD'}
+        self._name = None
+        #self._time = None
+        self._date = None
+        self._time_index = None
+        self._event_type = None
+
+    @property
+    def name(self): return self._name
+
+    @property
+    def time(self):
+        return self._date.strftime('%H:%M')
+
+    @property
+    def time_index(self): return self._time_index
+
+    @property
+    def date(self): return self._date
+
+    @property
+    def event_type(self): return self._event_type
 
     def echo(self):
-        r = requests.get('https://calendly.com/api/v1/echo', headers=self._api_key)
-        if json.loads(r.text)['email']:
+        r = requests.get('https://calendly.com/api/v1/echo', headers=self.api_key)
+        content = eval(r.text)
+        if content['email']:
             return True
         else:
             return False
 
-    def get_key(self):
-        print(self._api_key)
-        return self._api_key
-
     def get_subscriptions(self):
         if self.echo():
-            r = requests.get('https://calendly.com/api/v1/hooks', headers=self._api_key)
-            content = json.loads(r.text)
+            r = requests.get('https://calendly.com/api/v1/hooks', headers=self.api_key)
+            content = eval(r.text)
             if content['data']:
                 return content['data']
         else:
-            return ''
+            return None
+
+    def add_event(self, data):
+        data = self._parse_raw_data(data)
+        formatted_data = data['the_data']
+
+        if formatted_data['event'] == 'invitee.created':
+            formatted_data = formatted_data['payload']
+            self._name = formatted_data['invitee']['name']
+            self._date = self._parse_date(formatted_data)
+            self._time_index = self._get_time_index(formatted_data)
+            #self._time = self._parse_time(formatted_data)
+            self._event_type = formatted_data['event_type']['slug']
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def _parse_raw_data(data):
+        data = data.replace('null', 'None')
+        data = data.replace('true', 'True')
+        data = data.replace('false', 'False')
+        data = eval(data)
+        return data
+
+    @staticmethod
+    def _get_time_index(data):
+        return data['event']['start_time_pretty']
+
+    # slice string, current format is '03:30pm - Friday, January 26, 2018'
+    @staticmethod
+    def _parse_date(data):
+        event_time = data['event']['start_time_pretty']
+        return datetime.datetime.strptime(event_time, '%I:%M%p - %A, %B %d, %Y')
+
+
+
+
 
 
 
--- tests/test_calendly.py
+++ tests/test_calendly.py
@@ -5,16 +5,24 @@ from lib import calendly
 class TestCalendly(unittest.TestCase):
 
     def setUp(self):
-            self.w = calendly.Calendly()
+            self.w = calendly.CalendlyEvent()
 
     def test_echo(self):
         self.assertTrue(self.w.echo())
 
-    def test_get_key(self):
-        self.assertIsNotNone(self.w.get_key())
-
     def test_get_subscriptions(self):
-        self.assertIsNotNone(self.w.get_subscriptions())
+        self.assertTrue(self.w.get_subscriptions())
+
+    def test_getters(self):
+        with open('datafiles/raw_data') as content:
+            data = content.read()
+
+        self.w.add_event(data)
+        self.assertEqual(self.w.name, 'Test McTest')
+        self.assertEqual(self.w.time, '10:15')
+        #self.assertEqual(self.w.time_index, 7)
+        #self.assertEqual(self.w.date, 'Thursday, February 1, 2018')
+        self.assertEqual(self.w.event_type, 'mobilehandover')
 
 
 if __name__ == '__main__':
